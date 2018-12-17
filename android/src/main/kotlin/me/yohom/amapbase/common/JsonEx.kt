f0a352bbdef7775fd3724f24cc87bb50a716b23b
yohom <382146139@qq.com>
2018-12-17 16:33:35 +0800

    enhancement：混合使用fastjson和Gson

--- android/src/main/kotlin/me/yohom/amapbase/common/JsonEx.kt
+++ android/src/main/kotlin/me/yohom/amapbase/common/JsonEx.kt
@@ -2,11 +2,24 @@ package me.yohom.amapbase.common
 
 import com.alibaba.fastjson.JSON
 import com.alibaba.fastjson.TypeReference
+import com.google.gson.Gson
+import com.google.gson.GsonBuilder
+import com.google.gson.reflect.TypeToken
+
+val gson: Gson = GsonBuilder().serializeNulls().create()
 
 inline fun <reified K> String.parseJson(): K {
-    return JSON.parseObject(this, object : TypeReference<K>() {}.type)
+    return gson.fromJson(this, object : TypeToken<K>() {}.type)
 }
 
 fun Any.toJson(): String {
+    return gson.toJson(this)
+}
+
+inline fun <reified K> String.parseFastJson(): K {
+    return JSON.parseObject(this, object : TypeReference<K>() {}.type)
+}
+
+fun Any.toFastJson(): String {
     return JSON.toJSONString(this)
 }
