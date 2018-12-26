ee02391d15a8d74f07c51e72ce72a754d9581f2f
yohom <382146139@qq.com>
2018-12-26 16:57:52 +0800

    chore: export

--- lib/amap_base.dart
+++ lib/amap_base.dart
@@ -7,6 +7,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 
 export 'amap_base.dart';
+export 'src/common/permissions.dart';
 export 'src/location/amap_location.dart';
 export 'src/location/model/location.dart';
 export 'src/location/model/location_client_options.dart';
