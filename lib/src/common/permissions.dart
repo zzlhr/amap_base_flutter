77fc4d857afd96651a90d4e047874fa53c9ff640
yohom <382146139@qq.com>
2018-12-26 16:46:38 +0800

    feature：申请权限的接口

---
+++ lib/src/common/permissions.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/services.dart';
+
+class Permissions {
+  static Permissions _instance;
+
+  static const _permissionChannel = MethodChannel('me.yohom/permission');
+
+  Permissions._();
+
+  factory Permissions() {
+    if (_instance == null) {
+      _instance = Permissions._();
+      return _instance;
+    } else {
+      return _instance;
+    }
+  }
+
+  /// 请求地图相关权限
+  Future requestPermission() {
+    return _permissionChannel.invokeMethod('requestPermission');
+  }
+}
