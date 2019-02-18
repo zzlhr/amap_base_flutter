d4c2f8bf4f7b5f37d95e082a6e041bfd5e28afd9
yohom <382146139@qq.com>
2019-02-18 11:07:57 +0800

    enhancement: 迁移到androidx

--- android/src/main/kotlin/me/yohom/amapbase/common/PermissionEx.kt
+++ android/src/main/kotlin/me/yohom/amapbase/common/PermissionEx.kt
@@ -1,7 +1,7 @@
 package me.yohom.amapbase.common
 
 import android.Manifest
-import android.support.v4.app.ActivityCompat
+import androidx.core.app.ActivityCompat
 import me.yohom.amapbase.AMapBasePlugin
 
 fun Any.checkPermission() {
