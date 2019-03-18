519e37aea3ce4f008debc9f8191cb0a51761d170
yohom <382146139@qq.com>
2019-03-18 09:06:00 +0800

    enhancement: android: base: 迁移support库

--- base/android/src/main/kotlin/me/yohom/amapbase/AMapBasePlugin.kt
+++ base/android/src/main/kotlin/me/yohom/amapbase/AMapBasePlugin.kt
@@ -5,7 +5,7 @@ import android.app.Activity
 import android.app.Application
 import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.core.app.ActivityCompat
+import android.support.v4.app.ActivityCompat
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.PluginRegistry.Registrar
 import me.yohom.amapbase.map.AMapFactory
--- base/android/src/main/kotlin/me/yohom/amapbase/common/PermissionEx.kt
+++ base/android/src/main/kotlin/me/yohom/amapbase/common/PermissionEx.kt
@@ -1,7 +1,7 @@
 package me.yohom.amapbase.common
 
 import android.Manifest
-import androidx.core.app.ActivityCompat
+import android.support.v4.app.ActivityCompat
 import me.yohom.amapbase.AMapBasePlugin
 
 fun Any.checkPermission() {
