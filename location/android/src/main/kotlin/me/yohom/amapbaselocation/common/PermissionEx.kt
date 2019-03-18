f257b57a6f58930ea877d1b0eeaf258dde57f940
yohom <382146139@qq.com>
2019-03-18 09:11:48 +0800

    enhancement: android: location: 迁移support库

--- location/android/src/main/kotlin/me/yohom/amapbaselocation/common/PermissionEx.kt
+++ location/android/src/main/kotlin/me/yohom/amapbaselocation/common/PermissionEx.kt
@@ -1,7 +1,7 @@
 package me.yohom.amapbaselocation.common
 
 import android.Manifest
-import androidx.core.app.ActivityCompat
+import android.support.v4.app.ActivityCompat
 import me.yohom.amapbaselocation.AMapBaseLocationPlugin
 
 fun Any.checkPermission() {
